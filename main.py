74483f2ad5ae59bb77dda0bc560a2d911d99e591
quantdevlike <hamedabbaszadeh91@gmail.com>
2024-06-03 11:55:18 +0200

    Get news for stock with large move

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import os
+from datetime import datetime, timedelta
 
+NEWS_API_KEY = os.environ["NEWS_API_KEY"]
 STOCK_API_KEY = os.environ["STOCK_API_KEY"]
 
 STOCK = "TSLA"
@@ -9,14 +11,14 @@ NEWS_ALERT_THRESHHOLD = 0.02
 
 # Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-params = {
+params_stock_api = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
     "apikey": STOCK_API_KEY
 }
 
-response = requests.get(f"https://www.alphavantage.co/query", params=params)
-print(response.json())
+response = requests.get(f"https://www.alphavantage.co/query", params=params_stock_api)
+response.raise_for_status()
 time_series_data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in time_series_data.items()]
 
@@ -24,11 +26,26 @@ data_list = [value for (key, value) in time_series_data.items()]
 yesterday_close = float(data_list[-1]["4. close"])
 day_before_yesterday_close = float(data_list[-1]["4. close"])
 
-if abs(yesterday_close - day_before_yesterday_close) / day_before_yesterday_close > NEWS_ALERT_THRESHHOLD:
-    print("Get News")
 
-## STEP 2: Use https://newsapi.org
+# Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+def get_news():
+    yesterday = datetime.today() - timedelta(1)
+    params_news_api = {
+        "q": COMPANY_NAME,
+        "to": str(yesterday.date()),
+        "from": str(yesterday.date()),
+        "sortBy": "popularity",
+        "apikey": NEWS_API_KEY
+    }
+    news_request = requests.get("https://newsapi.org/v2/everything", params=params_news_api)
+    news_request.raise_for_status()
+    news = news_request.json()["articles"][:3]
+    return news
+
+
+if abs(yesterday_close - day_before_yesterday_close) / day_before_yesterday_close > NEWS_ALERT_THRESHHOLD:
+    news = get_news()
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
